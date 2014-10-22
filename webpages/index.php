96bab050bcb67b68f2a79334685b21d9fdb07ab8
caiwang <caiwang@tsinghua.org.cn>
2014-10-22 10:15:41 +0800

    修改GBK到utf8

--- webpages/index.php
+++ webpages/index.php
@@ -1,13 +1,5 @@
 <?php
-/* *
-* $Author ：PHPYUN开发团队
-*
-* 官网: http://www.phpyun.com
-*
-* 版权所有 2009-2014 宿迁鑫潮信息技术有限公司，并保留所有权利。
-*
-* 软件声明：未经授权前提下，不得用于商业运营、二次开发以及任何形式的再次发布。
-*/
+
 include(dirname(__FILE__)."/global.php");
 if($config['webcache']=="1"){
 	include_once(LIB_PATH."web.cache.php");
