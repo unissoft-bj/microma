d653628bc661c9a93bb188d1e6b6fde79fdc39aa
ycyn521 <ycyn521@qq.com>
2014-12-29 14:16:16 +0800

    提交订单信息时同时修改useraccounts和member

--- webpages/ma/caipiao_submit.php
+++ webpages/ma/caipiao_submit.php
@@ -5,75 +5,95 @@ require 'inc/function.inc.php';
 
 if($_POST){
 	
+	//useraccounts中的积分处理、userlog中的积分记录处理
+	$sql="select integral,intid from useraccounts where userid='".$_COOKIE['userid']."'";
+	$rs=$db->r($sql);
+	$integral_old =  $rs['integral'];
+	$intid = $rs['intid'];
+	$integral_new = $integral_old-1000;
+	//echo $integral_new;
+	//������㹻��һ�������һ�ʧ��
+	if ($integral_new>=0) {
+		/*�һ�
+				
+		1.����useraccounts �Ļ��
+		2.��Ӷһ���¼��userlog��
+		3.��ת����Ʒ����ҳ
+		*/
+		//echo "ok";
+		mysql_query("BEGIN"); //或者mysql_query("START TRANSACTION");
+			
+			
+			
+		$sql1="update useraccounts set integral=".$integral_new ." where userid='".$_COOKIE['userid']."'";
+		$rs1=$db->q($sql1);
+		$sql2="INSERT INTO userlog (userid,integral,dintegral,action,rectime)
+			VALUES ('".$_COOKIE['userid']."',".
+				$integral_old.",-1000,'兑换彩票：".$_GET['ssq_str']."','".dtime()."')";
 	
-	if ($infomore==1) {
-		//如果强制要求用户输入身份信息
-		echo "more";
-	}else{
-		//不要求用户输入身份信息
+		$rs2=$db->q($sql2);
+		
+		//如果需要输入用户信息，则将用户信息存入订单表，如果有原来的用户信息为空，则更新useraccounts表
+		if ($infomore==1) {
+			//如果强制要求用户输入身份信息
+			$sql_userinfo="update useraccounts set 
+					lname='".$_POST['username'] ."',
+					cid = '".$_POST['cid']."',
+					useremail1 = '".$_POST['email']."'  
+					 where userid='".$_COOKIE['userid']."'";
+			$db->q($sql_userinfo);
+			
+			$sql_member="update member set
+					username='".$_POST['username'] ."' 
+					 where uid='".$intid."'";
+			$db->q($sql_member);
+			//echo $rs_userinfo['intid'];
+			//die();
+			
+		}
+		
+		//将订单数据存入订单表
+		$pro_creattime=dtime();
+		$sql3="insert into `prodorder`
+				(`userid`, `username`, `prodcode`, `prodname`, `prodtype`, `prodspec`, `proddesp`, `recipaddr`, `recipname`, `recipphone1`, `recipphone2`, `rectime`, `recipemail`)
+		  values('".$_COOKIE['userid']."','".$_COOKIE['username']."','caip','caip','caip','".$_POST['ssq_str']."','123132123123',NULL,NULL,NULL,NULL,'".$pro_creattime."',NULL);
+		";
+		$rs3 = $db->q($sql3);
+			
+		//修改手机号
 		if ($_COOKIE['phone']==$_POST['phone']) {
 			//echo "meibian";
-			
+				
 		}else{
 			//echo "bian";
 			//如果输入手机号和useraccounts中的手机号不一样，则将新手机号更新到backphone字段中
 			$sql = "UPDATE useraccounts SET backphone = '".$_POST['phone']."' WHERE userid = '".$_COOKIE['userid']."' ";
 			$db->q($sql);
 		}
-				
-		//useraccounts中的积分处理、userlog中的积分记录处理
-		$sql="select integral from useraccounts where userid='".$_COOKIE['userid']."'";
-		$rs=$db->r($sql);
-		$integral_old =  $rs['integral'];
-		$integral_new = $integral_old-1000;
-		//echo $integral_new;
-		//������㹻��һ�������һ�ʧ��
-		if ($integral_new>=0) {
-			/*�һ�
-			 	
-			1.����useraccounts �Ļ��
-			2.��Ӷһ���¼��userlog��
-			3.��ת����Ʒ����ҳ
-			*/
-			//echo "ok";
-			mysql_query("BEGIN"); //或者mysql_query("START TRANSACTION");
-			
-			$sql1="update useraccounts set integral=".$integral_new ." where userid='".$_COOKIE['userid']."'";
-			$rs1=$db->q($sql1);
-			$sql2="INSERT INTO userlog (userid,integral,dintegral,action,rectime)
-			VALUES ('".$_COOKIE['userid']."',".
-					$integral_old.",-1000,'兑换彩票：".$_GET['ssq_str']."','".dtime()."')";
-				
-			$rs2=$db->q($sql2);		
-			
-			//将订单数据存入订单表
-			$pro_creattime=dtime();
-			$sql3="insert into `prodorder`
-				(`userid`, `username`, `prodcode`, `prodname`, `prodtype`, `prodspec`, `proddesp`, `recipaddr`, `recipname`, `recipphone1`, `recipphone2`, `rectime`, `recipemail`)
-		  values('".$_COOKIE['userid']."','".$_COOKIE['username']."','caip','caip','caip','".$_POST['ssq_str']."','123132123123',NULL,NULL,NULL,NULL,'".$pro_creattime."',NULL);
-		";
-			$rs3 = $db->q($sql3);
-			
-			if($rs1&&$rs2&&$rs3){
-				mysql_query("COMMIT");
-				$_SESSION['jifen']=$integral_new;
-				header("location: caipiao.php?point=兑换成功，消耗积分1000");
-				die();
-				echo '提交成功。';
-			}else{
-				mysql_query("ROLLBACK");
-				header("location: caipiao.php?point=兑换失败，数据异常");
-				die();
-				echo '数据回滚。';
-			}
-			
-				
-		}else {
-			//��ת����Ʒ����ҳ
-			header("location: caipiao.php?point=积分不足，兑换失败");
-			//msg("兑积分不足 兑换失败",$lailu."?&point=积分不足 兑换失败",1);
+		
+		
+		if($rs1&&$rs2&&$rs3){
+			mysql_query("COMMIT");
+			$_SESSION['jifen']=$integral_new;
+			header("location: caipiao.php?point=兑换成功，消耗积分1000");
+			die();
+			echo '提交成功。';
+		}else{
+			mysql_query("ROLLBACK");
+			header("location: caipiao.php?point=兑换失败，数据异常");
+			die();
+			echo '数据回滚。';
 		}
+			
+	
+	}else {
+		//��ת����Ʒ����ҳ
+		header("location: caipiao.php?point=积分不足，兑换失败");
+		die();
+		//msg("兑积分不足 兑换失败",$lailu."?&point=积分不足 兑换失败",1);
 	}
+	
+	
 	
 	die();
 	
@@ -117,10 +137,7 @@ if($_POST){
       
     <p>
     
-    <p>
-    
-      发送彩票标识码短信到：
-    </p>
+    <p>发送彩票标识码短信到：    </p>
     <p>    
       <input value="<?php echo $_COOKIE['phone'];?>" name="phone" id="phone" type="text" class="input-common placeholder" placeholder="请输入手机号" onblur="return getInfoByRegPhone(this.value);"/>
     </p>
@@ -130,19 +147,30 @@ if($_POST){
    //$sql="select integral from useraccounts where userid='".$_COOKIE['userid']."'";
     $sql = "select * from userinfochk where obj='".$_COOKIE['userid']."' and useraction='integral2lottery' and stat='100'";
     
-    $rs=$db->r($sql);
-    if ($rs['info']=="caipiao" && $rs['op']=="100" & $rs['action']==100) {
     
+    $rs=$db->r($sql);
+//     echo $rs['info'];
+//     echo $rs['op'];
+//     echo $rs['action'];
+    //die();
+    if ($rs['info']=="caipiao" && $rs['op']=="100" & $rs['action']==100) {
+    	$sql2 = "select * from useraccounts where userid='".$_COOKIE['userid']."'";
+    	//echo $sql2;
+    	$rs2 = $db->r($sql2);
+    	//die();
     ?>
+    <p>姓名：    </p>
     <p>
       <input name="infomore" id="username" type="hidden"class="input-common placeholder" placeholder="用户名" value="1"/>
-      <input name="username" id="username" type="text"class="input-common placeholder" placeholder="用户名" value=""/>
+      <input name="username" id="username" type="text"class="input-common placeholder" placeholder="用户名" value="<?php echo $rs2['lname']?>" />
     </p>
+    <p>邮箱：    </p>
     <p>
-      <input name="email" id="email" type="text"class="input-common placeholder" placeholder="email" value=""/>
+      <input name="email" id="email" type="text"class="input-common placeholder" placeholder="email" value="<?php echo $rs2['useremail1']?>"/>
     </p>
+    <p>证件号码：    </p>
     <p>
-      <input name="cid" id="cid" type="text"class="input-common placeholder" placeholder="身份证号"  value=""/>
+      <input name="cid" id="cid" type="text"class="input-common placeholder" placeholder="身份证号"  value="<?php echo $rs2['cid']?>"/>
     </p>
     
     <?php 
@@ -151,7 +179,7 @@ if($_POST){
     	
     }
     ?>
-    <input type="submit" name="submit" value="签到" class="btn-large" />
+    <input type="submit" name="submit" value="提交" class="btn-large" />
   </form>
   
   
