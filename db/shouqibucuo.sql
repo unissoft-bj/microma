a0e6f1e63b84258eed7968f94600e590d1f79a73
caiwang <caiwang@tsinghua.org.cn>
2015-01-19 22:46:43 +0800

    update shouqibucuo.sql

--- db/shouqibucuo.sql
+++ db/shouqibucuo.sql
@@ -12,9 +12,9 @@ MySQL - 5.0.90-community-nt : Database - ihost-ma
 /*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
 /*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
 /*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
-CREATE DATABASE /*!32312 IF NOT EXISTS*/`ihost-ma` /*!40100 DEFAULT CHARACTER SET latin1 */;
+#CREATE DATABASE /*!32312 IF NOT EXISTS*/`ihost-ma` /*!40100 DEFAULT CHARACTER SET latin1 */;
 
-USE `ihost-ma`;
+USE `wlsp`;
 
 /*Table structure for table `shouqibucuo` */
 
