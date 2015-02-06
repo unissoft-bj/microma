2e3c0ec2943ddb1ae5b8ca048b0c20bfcda25553
ycyn521 <ycyn521@qq.com>
2015-02-06 15:55:31 +0800

    matrix wifi介绍和mirror界面修改

--- webpages/templates_c/%%9E^9EC^9ECA2D72%%index.htm.php
+++ webpages/templates_c/%%9E^9EC^9ECA2D72%%index.htm.php
@@ -1,4 +1,4 @@
-<?php /* Smarty version 2.6.26, created on 2014-11-29 14:05:37
+<?php /* Smarty version 2.6.26, created on 2015-02-06 14:58:05
          compiled from wap/index.htm */ ?>
 <?php $_smarty_tpl_vars = $this->_tpl_vars;
 $this->_smarty_include(array('smarty_include_tpl_file' => ($this->_tpl_vars['wapstyle'])."/header.htm", 'smarty_include_vars' => array()));
@@ -9,35 +9,31 @@ unset($_smarty_tpl_vars);
 <!-- header  class="index_post_hot_h1"><a href="javascript:;">欢迎来宾</a></header> -->
 <table class="ht">
 <tbody>
-<tr>
-
-<td><b>时间</b></td>
-<td><b>地点</b></td>
-<td><b>内容</b></td>
-<td><b>主讲人</b></td>
-<td><b>备注</b></td>
-
-</tr>
-<tr>
-
-<td><a href="" title="">2014-12-12</a></td>
-<td><a href="" title="">2号会客厅</a></td>
-<td><a href="" title="">移动互联网与电子商务</a></td>
-<td><a href="" title="">马云</a></td>
-<td><a href="" title="">位置有限</a></td>
-
+<tr align='left'>
+<td width="60"><b>公益拇指</b></td>
+<td align='left'><div align='left'>通过答题参与公益活动。出题方为您提供活动执行所需的费用。</div></td>
 </tr>
 
-<tr>
-
-<td><a href="" title="">2014-12-12</a></td>
-<td><a href="" title="">4号会客厅</a></td>
-<td><a href="" title="">移动互联网与社交媒体</a></td>
-<td><a href="" title="">麻花藤</a></td>
-<td><a href="" title="">位置有限</a></td>
-
+<tr align='left'>
+<td width="60"><b>积分礼包</b></td>
+<td align='left'><div align='left'>管理您的积分。用积分参与兑换、捐助等活动。</div></td>
 </tr>
-<tr>
+
+<tr align='left'>
+<td width="60"><b>手气不错</b></td>
+<td align='left'><div align='left'>是我们为您准备的抽奖环节，欢迎一试身手！</div></td>
+</tr>
+
+<tr align='left'>
+<td width="60"><b>精彩视频</b></td>
+<td align='left'><div align='left'>有趣的视频分享，一天好心情！</div></td>
+</tr>
+
+<tr align='left'>
+<td width="60"><b>签到上网</b></td>
+<td align='left'><div align='left'>签到可自由访问Internet。首次签到的用户将额外获赠积分。</div></td>
+</tr>
+
 
 </tbody>
 </table>
